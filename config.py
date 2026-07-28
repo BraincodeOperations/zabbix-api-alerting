6808cd7d5d79b175a7c51da63131d55315fa3def
Girindra Wijaya <wijayaindrapbg@gmail.com>
2026-07-28 17:16:23 +0700

    change host

--- config.py
+++ config.py
@@ -1,22 +1,16 @@
 TOKEN = "8716850157:AAH-u81td6OvXdzCKDBmHf1AwwqeTDLFoZE"
-ZABBIX_AUTH = "f9e24e8d1ba2323c4701ad30ae5333e0"
-ZABBIX_URL = "http://172.16.11.34/zabbix/api_jsonrpc.php"
-
-HOST = {}
-ITEMIDS_CPU = {}
-ITEMIDS_DISK = {}
-ITEMIDS_MEM = {}
-
-ITEMIDS_PVE03 = {
-    "CPU Utilization": "50806",
-    "Memory Utilization": "50807",
-    "sda: Disk utilization": "50884",
-    "sdb: Disk utilization": "50877"
-}
-
+ZABBIX_AUTH = "04f83536c0c01db661af34941194bd1d"
+ZABBIX_URL = "http://192.168.40.191/zabbix/api_jsonrpc.php"
 
 CHAT_ID = "-5572231182"
 
 CPU_THRESHOLD = 70
 MEMORY_THRESHOLD = 70
 DISK_THRESHOLD = 70
+
+ITEMIDS_PVE03 = {
+    "CPU Utilization": "50806",
+    "Memory Utilization": "50807",
+    "sda: Disk utilization": "50904",
+    "sdb: Disk utilization": "50897"
+}
