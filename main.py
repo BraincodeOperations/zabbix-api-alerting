5f1851c331027d3de24ee88508e53c29cf63402f
Girindra Wijaya <wijayaindrapbg@gmail.com>
2026-07-28 19:00:40 +0700

    edit dikit

--- main.py
+++ main.py
@@ -16,5 +16,7 @@ def run_bot():
 
 
 if __name__ == "__main__":
-    threading.Thread(target=run_bot, daemon=True).start()
-    main()
+    threading.Thread(target=main, daemon=True).start()
+
+    # bot dijalankan di main thread
+    run_bot()
